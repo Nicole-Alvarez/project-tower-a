11774f86da2d03dc0b10dbe083bbdd5003719ce2
Nicole-Alvareze <nicole.largo.alvarez@gmail.com>
2025-03-01 14:45:53 +0800

    test push for vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Neek
+            Neeks
           </a>
         </div>
       </main>
