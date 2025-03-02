a2b91f342cec2ad4fa85871cc99d272f841698bb
Nicole-Alvareze <nicole.largo.alvarez@gmail.com>
2025-03-02 16:32:26 +0800

    test: vercel deploy

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,56 +1,14 @@
-import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import { prettyJSON } from "hono/pretty-json";
-import type { JwtVariables } from "hono/jwt"; 
-import authRoutes from "./routes/auth.routes";
-import { authMiddleware } from "./helpers/middleware"; 
-import userRoutes from "./routes/user.route"; 
-import publicTestRoutes from "./routes/public-test.routes";
-import mobRoutes from "./routes/mob.route";
-
-type Variables = JwtVariables;
+import { handle } from "@hono/node-server/vercel";
 
 const app = new Hono();
 
-// middlewares
-// app.use("/*", cors());
-app.use(
-  cors({
-    origin: "*", // Allow all origins
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Allow cookies/auth headers if needed
-  })
-);
-app.use(logger());
-app.use(prettyJSON());
-app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
-
-// Public routes
-app.route('/test', publicTestRoutes);
-app.route('/auth', authRoutes); 
-
-// /api/v1 instance
-const apiv1 = new Hono<{ Variables: Variables }>();
-
-apiv1.get("/", (c) => {
-  return c.text("api v1");
+app.get("/", (c) => {
+	return c.text("Hello, Vercel!");
 });
 
-apiv1.route('/users', userRoutes); 
-apiv1.route('/mobs', mobRoutes); 
-
-app.use("/api/v1/*", authMiddleware);
-
-// api/v1 route
-app.route("/api/v1/", apiv1);
-
-const port = 3001;
-console.log(`Server is running on port ${port}`);
-
-serve({
-  fetch: app.fetch,
-  port,
+app.get("/hello/:name", (c) => {
+	return c.text(`Hello, ${c.req.param("name")}!`);
 });
+
+export default handle(app);
